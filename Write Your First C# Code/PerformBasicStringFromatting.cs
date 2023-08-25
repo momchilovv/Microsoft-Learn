14ea50f975290566e28a6afdb316e6ea45fe4189
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2023-08-25 20:58:14 +0200

    Uploading Perform Basic String Formatting.

---
+++ Write Your First C# Code/PerformBasicStringFromatting.cs
@@ -0,0 +1,7 @@
+﻿string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine("View English output:\r\n" + $@"  c:\Exercise\{projectName}\data.txt" + "\r\n");
+
+Console.WriteLine($"{russianMessage}:\r\n" + $@"  c:\Exercise\{projectName}\ru-RU\data.txt" + "\r\n");
