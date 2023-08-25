e6d02cbfac6b8a442755f60c09d8dfd44834e648
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2023-08-25 20:59:21 +0200

    Uploading Student Grading Application.

---
+++ Write Your First C# Code/StudentGradingApplication.cs
@@ -0,0 +1,25 @@
+﻿var sophia = new List<int>()
+{
+    93, 87, 98, 95, 100
+};
+
+var nicolas = new List<int>()
+{
+    80, 83, 82, 88, 85
+};
+
+var zahirah = new List<int>()
+{
+    84, 96, 73, 85, 79
+};
+
+var jeong = new List<int>()
+{
+    90, 92, 98, 100, 97
+};
+
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine($"Sophia:\t\t {sophia.Average():f1} \tA");
+Console.WriteLine($"Nicolas:\t {nicolas.Average():f1} \tB");
+Console.WriteLine($"Zahirah:\t {zahirah.Average():f1} \tB");
+Console.WriteLine($"Jeong:\t\t {jeong.Average():f1} \tA");
