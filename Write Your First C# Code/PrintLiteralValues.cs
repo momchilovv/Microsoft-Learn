59278164e4a79c56452b1ae941193b038cc41bba
Tsvetelin Tsvetanov <momchilovv@hotmail.com>
2023-08-25 20:58:46 +0200

    Uploading Print Literal Values.

---
+++ Write Your First C# Code/PrintLiteralValues.cs
@@ -0,0 +1,5 @@
+﻿string name = "Bob";
+int inboxMessages = 3;
+double temperature = 34.4;
+
+Console.Write($"Hello, {name}! You have {inboxMessages} in your inbox. The temperature is {temperature} celsius.");
